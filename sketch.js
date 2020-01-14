f6c42321c4f04f820ae359b7470ed7179590ca95
MakarCodes <jarek.strupek@wp.pl>
2020-01-14 22:31:31 +0100

    adding reset funcionality and eventlistener changing cell background

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('#grid-container');
+const resetButton = document.querySelector('#reset');
 
 function createGrid(rows, columns){
     // for index 0, next loop goes 'columns' number times
@@ -12,13 +13,34 @@ function createGrid(rows, columns){
 }
 
 function defaultGrid(){
-    createGrid(5,5);
+    createGrid(16,16);
     gridContainer.style.display = 'grid';
     gridContainer.style.gridGap = '1px';
-    gridContainer.style.gridTemplateColumns = 'repeat(5, 99.2px)';
-    gridContainer.style.gridTemplateRows = 'repeat(5, 99.2px)';
+    gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+    gridContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
 }
 
 defaultGrid();
+
+gridContainer.addEventListener('mouseover', e => {
+    if(e.target.classList.contains('cell')){
+        e.target.style.backgroundColor = 'black';
+    }
+})
+
+resetButton.addEventListener('click', e => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach( function(cell) {
+        if (!(cell.getAttribute('style') == null)) {
+            cell.removeAttribute('style');
+    }})
+    // for (let i = 0; i < cells.length; i++) {
+    //     if (!((cells[i]).getAttribute('style') == null)) {
+    //         cells[i].removeAttribute('style');
+    //     }
+    // }
+});
+
+
 
 
